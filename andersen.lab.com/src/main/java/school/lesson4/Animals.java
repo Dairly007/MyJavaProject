4cd1316d20b7571ce011c48ca681625429e0916c
Dairly <megachekal@mail.ru>
2022-02-20 03:25:11 +0300

    Lesson 5 1st commit

--- andersen.lab.com/src/main/java/school/lesson4/Animals.java
+++ andersen.lab.com/src/main/java/school/lesson4/Animals.java
@@ -1,42 +1,79 @@
 package school.lesson4;
 
-public abstract class Animals extends Main{
+public abstract class Animals extends Main
+{
+	protected static int countAnimals = 0;
 	protected int length;
 	protected String name;
 	protected int runLimit;
 	protected int swimLimit;
 	protected int appetite;
+	public abstract class Animals extends Main{
+		protected int length;
+		protected String name;
+		protected int runLimit;
+		protected int swimLimit;
+		protected int appetite;
 
-	public abstract void animal(String name, int length, int appetite);
-	public abstract int swim(int length);
-	public abstract int run(int length);
 
-}
+		public Animals(String name, int runLimit, int swimLimit, int appetite)
+		{
+			this.name = name;
+			this.runLimit = runLimit;
+			this.swimLimit = swimLimit;
+			this.appetite = appetite;
+			System.out.println("Current amount of the animals is: " + (countAnimals += 1));
+		}
+		public Animals(String name, int runLimit, int swimLimit, int appetite)
+		{
+			this.name = name;
+			this.runLimit = runLimit;
+			this.swimLimit = swimLimit;
+			this.appetite = appetite;
+			System.out.println("Current amount of the animals is: " + (countAnimals += 1));
+		}
+		public abstract void animal(String name, int length, int appetite);
+		public abstract int swim(int length);
+		public abstract int run(int length);
 
-/*
-	private class Plate
-	{
-		private int food;
+		public int swim(int length)
+		{
+			public int swim(int length)
+			public class Plate
+			{
+		this.length = length;
+		return 0;
+			}
+			private int food;
+		}
 
+		public int run(int length)
+		{
+			this.length = length;
+			return 0;
+		}
+		public int run(int length)
+		{
+			this.length = length;
+			return 0;
+		}
+
+		public static class Plate
+		{
+			public static int food;
+			public static class Plate
+			{
+				public static int food;
+
+				public Plate(int food)
+				{
+					this.food = food;
+				}
+			}
+		}
 		public Plate(int food)
 		{
 			this.food = food;
 		}
-
-		public void decreaseFood(int n)
-		{
-			System.out.println();
-		}
-
-		public void info()
-		{
-			System.out.println("plate:" + food);
-		}
-		public void eat(){
-			System.out.println("Dog is eating");
-		}
-		public int run(int limit){
-			System.out.println("Dog is running");
-		}
+	}
 }
-*/
--- andersen.lab.com/src/main/java/school/lesson4/Dog.java
+++ andersen.lab.com/src/main/java/school/lesson4/Dog.java
@@ -2,7 +2,26 @@ package school.lesson4;
 
 public class Dog extends Animals
 {
+	public static int countDogs = 0;
 
+	public Dog(String name, int runLimit, int swimLimit, int appetite)
+	{
+		super(name, runLimit, swimLimit, appetite);
+		System.out.println("Current amount of the dogs is: "+(countDogs+1));
+	}
+
+	@Override
+	public int swim(int length)
+	{
+		swimLimit = 10;
+		System.out.println(name + " is swimming at: ");
+		if (length > swimLimit)
+			return swimLimit;
+		else if (length <= swimLimit && length >= 0)
+			return length;
+		else
+			throw new IllegalStateException("Error: Integers can't have negative value");
+	}
 	@Override
 	public void animal(String name, int length, int appetite)
 	{
@@ -14,7 +33,12 @@ public class Dog extends Animals
 	@Override
 	public int swim(int length)
 	{
-		super.swimLimit = 10;
+		swimLimit = 10;
+		System.out.println(name + " is swimming at: ");
+		if (length > swimLimit)
+			return swimLimit;
+		else if (length <= swimLimit && length >= 0)
+			super.swimLimit = 10;
 		System.out.println(super.name + " is swimming at: ");
 		if (length > super.swimLimit)
 			return super.swimLimit;
@@ -27,7 +51,25 @@ public class Dog extends Animals
 	@Override
 	public int run(int length)
 	{
-		super.runLimit = 500;
+		runLimit = 500;
+		System.out.println(name + " is running at: ");
+		if (length > runLimit)
+			return runLimit;
+		else if (length < runLimit && length > 0)
+			return length;
+		else
+			throw new IllegalStateException("Error: Integers can't have negative value");
+	}
+}
+	@Override
+	public int run(int length)
+	{
+		runLimit = 500;
+		System.out.println(name + " is running at: ");
+		if (length > runLimit)
+			return runLimit;
+		else if (length < runLimit && length > 0)
+			super.runLimit = 500;
 		System.out.println(super.name + " is running at: ");
 		if (length > super.runLimit)
 			return super.runLimit;
@@ -37,3 +79,4 @@ public class Dog extends Animals
 			throw new IllegalStateException("Error: Integers can't have negative value");
 	}
 }
+
