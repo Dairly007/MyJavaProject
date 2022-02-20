77d88a3798109ae63a1a0259f98d0efc6dc09faa
Dairly <megachekal@mail.ru>
2022-02-20 16:04:07 +0300

    Lesson 4  19.02.2022

--- andersen.lab.com/src/main/java/school/lesson5/Apple.java
+++ andersen.lab.com/src/main/java/school/lesson5/Apple.java
@@ -1,6 +1,28 @@
 package school.lesson5;
 
-public class Apple <F extends Fruit>
-{
+import java.util.Arrays;
 
+public class Apple<F extends Fruit>
+{
+	int amount;
+	String a;
+
+	public Apple(String a)
+	{
+		this.a = a;
 	}
+
+	public static void main(String[] args)
+	{
+		Apple a0 = new Apple("russia");
+		Apple a1 = new Apple("usa");
+		Apple aTmp = new Apple("swap");
+		Apple a[] = {a0, a1};
+		System.out.println(Arrays.deepToString(a));
+		Apple tmp = a[1];
+		Apple tmp1 = a[0];
+		a[0] = a[1];
+		a[1] = tmp1;
+		System.out.println(Arrays.deepToString(a));
+	}
+}
