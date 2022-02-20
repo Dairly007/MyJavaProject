69742a4043dfd09697336545026233a334ced6ec
Dairly <megachekal@mail.ru>
2022-02-20 16:05:21 +0300

    Lesson 4  19.02.2022

---
+++ andersen.lab.com/src/main/java/school/lesson5/ChangeArr.java
@@ -0,0 +1,34 @@
+package school.lesson5;
+
+public class ChangeArr
+{
+	String name;
+
+	public ChangeArr(String name)
+	{
+		this.name = name;
+	}
+
+	public static void changeArr  (ChangeArr[] arr, int a, int b){
+
+		for (ChangeArr changeArr : arr)
+		{
+			System.out.println(changeArr.name);
+		}
+		ChangeArr tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+		for (ChangeArr changeArr : arr)
+		{
+			System.out.println(changeArr.name);
+		}
+	}
+	public static void main(String[] args)
+	{
+		ChangeArr a0 = new ChangeArr("Russia");
+		ChangeArr a1 = new ChangeArr("USA");
+		ChangeArr[] a = {a0, a1};
+		changeArr( a, 0, 1);
+		System.out.println((a[0].name));
+	}
+}
